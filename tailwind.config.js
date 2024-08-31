4147bef43ee435b3ebcf991f65e8b18078caa5b2
dhanarrizky <dhanarrizky23@gmail.com>
2024-08-31 15:28:14 +0700

    updateing code for configuration css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  // content: ["./src/one.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
